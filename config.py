c3da42294efd0c1faf812cf0bff24fe6157c4931
ahrtechandseries-wq <ahrtechandseries@gmail.com>
2026-01-02 19:04:52 +0600

    Add configuration settings for bot and database

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+BOT_TOKEN = os.getenv("8445895843:AAH_mWI4tBRsTs0fGbWIeqg80uNPEfyK3QQ")
+API_ID = int(os.getenv("20726200"))
+API_HASH = os.getenv("5e927fe061c2f988a843053b67f47da9")
+MONGO_URI = os.getenv("mongodb+srv://anik123:<db_rashni2215>@cluster0.vm9te27.mongodb.net/?appName=Cluster0
+")
+
+AUTO_DELETE = int(os.getenv("AUTO_DELETE", 1800))  # 30 minutes
